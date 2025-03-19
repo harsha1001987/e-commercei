59e8616497b98f7ab92666db0a13d970e1d076c7
harsha1001987 <matlaharshavardhanaraju@gmail.com>
2025-03-19 11:13:02 +0530

    cm

--- fontend/src/routes/routes.jsx
+++ fontend/src/routes/routes.jsx
@@ -1,4 +1,12 @@
-import LoginPage from "../page/LoginPage/Login"
-import SignupPage from "../page/SignupPage/SignupPage"
-import home from '../page/home'
-export {LoginPage,SignupPage,home}
+// routes/Routes.jsx
+import LoginPage from '../pages/Login'; // Correct path to Login.jsx
+import Signup from '../pages/Signup';
+import ActivationPage from '../pages/Activation'; // Correct path to Activation.jsx
+import HomePage from '../pages/HomePage';
+import CreateProduct from '../pages/CreateProduct'
+import MyProduct from '../pages/myProduct'
+import ProductDetail from '../pages/ProductDetail';
+import Cart from '../pages/cart';
+import Profile from '../pages/profile';
+import CreateAddress from '../pages/createAddress';
+export { LoginPage,Signup,ActivationPage, HomePage,CreateProduct, MyProduct ,ProductDetail , Cart,Profile,CreateAddress }; // Named export
